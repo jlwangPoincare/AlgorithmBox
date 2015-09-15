f6ae85edfa8054af574f677b530e8377e5fba06e
jlwangPoincare <jlwang.poincare@gmail.com>
2015-09-14 23:08:10 -0700

    Finished gcd part.

--- ex_euclid/gcd.py
+++ ex_euclid/gcd.py
@@ -2,7 +2,8 @@ def gcd(a, b):
 """
 A function used to evaluate the greatest common divisor.
 Input two integers, a and b. Do not need to be in order.
-Program will check input types.
+Program will check input types. Nonpositive input would
+not work.
 """
     if not isinstance(a, int):
         print 'Noninteger input a'
@@ -10,8 +11,14 @@ Program will check input types.
     if not isinstance(b, int):
         print 'Noninteger input b'
         return None
+    if a <= 0 or b <= 0:
+        print 'Not positive integer inputted'
+        return None
+
+    # Key part
     r = a % b
     if r == 0:
         return b
     return gcd(b, r)
+    # Key part end
 
