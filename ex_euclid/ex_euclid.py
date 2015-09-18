3e61076925bca4b108f26eb31d384348046e6c10
jlwangPoincare <jlwang.poincare@gmail.com>
2015-09-17 18:02:03 -0700

    Package

--- ex_euclid/ex_euclid.py
+++ ex_euclid/ex_euclid.py
@@ -1,3 +1,29 @@
+# This file can be used as a package to be imported
+# Type "import ex_euclid"
+def gcd(a, b):
+    """
+    A function used to evaluate the greatest common divisor.
+    Input two integers, a and b. Do not need to be in order.
+    Program will check input types. Nonpositive input would
+    not work.
+    """
+    if not isinstance(a, int):
+        print 'Noninteger input a'
+        return None
+    if not isinstance(b, int):
+        print 'Noninteger input b'
+        return None
+    if a <= 0 or b <= 0:
+        print 'Not positive integer inputted'
+        return None
+
+    # Key part
+    r = a % b
+    if r == 0:
+        return b
+    return gcd(b, r)
+    # Key part end
+
 def ex_euclid(a1, b1):
     """
     A function used to implement the extended Euclidean algorithm.
@@ -28,4 +54,27 @@ def ex_euclid(a1, b1):
     X1 = tx1 * Y2
     Y1 = ty1 * Y2 + X2
     return (X1, Y1, g)
+    # Key part end
+
+def m_inverse(element, modulous):
+    """
+    A function used to calculate the modular multiplicative inverse.
+    Using the extended Euclidean algorithm.
+    """
+    if not isinstance(element, int):
+        print 'Noninteger input element'
+        return None
+    if not isinstance(modulous, int):
+        print 'Noninteger input modulous'
+        return None
+    if element >= modulous or modulous <= 0:
+        print 'Bad integer inputted'
+        return None
+
+    # Key part
+    (temp1, temp_d, temp3) = ex_euclid(modulous, element)
+    if temp_d * element % modulous != 1:
+        print 'd failed'
+        return None
+    return temp_d
     # Key part end
