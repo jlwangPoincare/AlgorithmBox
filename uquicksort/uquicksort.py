cb9815665755a0711669c44e44351bc579a12e70
jlwangPoincare <jlwang.poincare@gmail.com>
2015-09-19 00:01:59 -0700

    begin quick sort in python

---
+++ uquicksort/uquicksort.py
@@ -0,0 +1,9 @@
+def uqsort(given_list):
+    """
+    Take a given list and quick sort it, return a new, sorted list?
+    """
+    pass
+
+sample_list = ['s', 'a', 'd', 'l', 'e', 'r', 'n', 'z', 't', 'b']
+print sample_list
+#print uqsort(sample_list)
