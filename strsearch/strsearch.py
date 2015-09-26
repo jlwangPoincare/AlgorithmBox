16031f2fa37a4d0f4d37fb01457e1b488748631c
jlwangPoincare <jlwang.poincare@gmail.com>
2015-09-26 16:13:00 -0700

    initial

---
+++ strsearch/strsearch.py
@@ -0,0 +1,8 @@
+def plain(instring, pattern):
+    """
+    A function uses plain string searching algorithm.
+    Input a string, and a pattern to be searched.
+    Return the index where the whole pattern is found for the first time.
+    If pattern not found, return -1.
+    """
+    pass
