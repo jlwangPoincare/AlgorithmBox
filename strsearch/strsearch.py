22f30619e88a88537dedebd4b478617796f51df8
jlwangPoincare <jlwang.poincare@gmail.com>
2015-09-28 14:15:34 -0700

    Add some comments to plain, start kmp

--- strsearch/strsearch.py
+++ strsearch/strsearch.py
@@ -8,16 +8,23 @@ def plain(instring, pattern):
     length_instring = len(instring)
     length_pattern = len(pattern)
     for i in range(length_instring - length_pattern + 1):
+        # search from instring[0] to instring[-length_pattern] for the pattern[0]
         for j in range(length_pattern):
+            # j starts from 0, if pattern[0] is not matched, break
+            # i stays at the same position all the way, moves j only
             if instring[i + j] != pattern[j]:
                 break
-            #else:
-                #pass
-        else: # The loop ends normally
+        else:
+            # The loop ends normally, no break, which means the whole pattern is found
             return i
-    else: # The outer loop ends normally, doesn't find the pattern
+            # instring[i] == pattern[0], so return i
+        # no need to add a break statement here
+    else:
+        # The outer loop ends normally, pattern not found
         return -1
+    # In this program, for-else statements are useful
 
-# This looks good
+#print plain('incsearch', 'cso')
 
-print plain('incsearch', 'cso')
+def kmp_search(instring, pattern):
+    pass
