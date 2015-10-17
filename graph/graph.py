1bf5d49b37a0ca43f5804c8bbc7d597ed3fcf9a6
jlwangPoincare <jlwang.poincare@gmail.com>
2015-10-16 22:35:36 -0700

    edges

--- graph/graph.py
+++ graph/graph.py
@@ -4,7 +4,7 @@
 class Graph(object):
     """Necessary data and methods of a graph object."""
 
-    def __init__(self, edge = list(), vertex_num = 0):
+    def __init__(self, vertex_num = 0, edge = list()):
         self.vertex = range(1, vertex_num + 1)
         self.edge = edge[:]
         return
@@ -21,11 +21,32 @@ class Graph(object):
         self.vertex.append(num)
         return
 
-mygraph = Graph([(1, 2), (2, 3), (1, 3)], 3)
+    def add_edge(self, edge_tuple):
+        if edge_tuple in self.edge:
+            print 'This edge already exist'
+            return
+        self.edge.append(edge_tuple)
+        return
+
+    def delete_edge(self, edge_tuple):
+        if edge_tuple in self.edge:
+            self.edge.remove(edge_tuple)
+            return
+        print 'This edge does not exist'
+        return
+
+
+mygraph = Graph(3, [(1, 2), (2, 3), (1, 3)])
 print mygraph
 
 mygraph.add_vertex(3)
 print mygraph
 
 mygraph.add_vertex(4)
+print mygraph
+
+mygraph.add_edge((3, 4))
+print mygraph
+
+mygraph.delete_edge((2, 3))
 print mygraph
